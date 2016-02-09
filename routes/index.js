7fce569fd94d62eed71ac34a6459dd671d2177c0
lsh9907 <lsh9907@hotmail.com>
2016-02-09 17:23:52 -0500

    Second commit (1. complete)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
-var router = express.Router();
+var app = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET Home page. */
+app.get('/', function(req, res, next) {
+  res.render('home', { title: 'Home' });
 });
 
-module.exports = router;
+/* GET Contact Me page. */
+app.get('/contactme', function(req, res, next) {
+  res.render('contactme', { title: 'Contact Me' });
+});
+
+// make this file visible within the rest of the application
+module.exports = app;

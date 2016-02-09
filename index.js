20b5a7480420ca77b17afcab8ca07f57f9c94904
lsh9907 <lsh9907@hotmail.com>
2016-02-09 16:49:13 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Sunghie on 2/9/2016.
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});

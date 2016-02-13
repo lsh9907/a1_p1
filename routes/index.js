7036167db89579535d00b4a9b28af982ed915191
lsh9907 <lsh9907@hotmail.com>
2016-02-12 20:32:10 -0500

    a1_p2 draft1

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,31 @@
+/* Connect and activate Express.js */
 var express = require('express');
 var app = express.Router();
 
-/* GET Home page. */
+/* GET Home page with title */
 app.get('/', function(req, res, next) {
   res.render('home', { title: 'Home' });
 });
 
-/* GET Contact Me page. */
+/* GET About Me page with a title */
+app.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'About Me' });
+});
+
+/* GET Projects page with a title */
+app.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET Services page with a title */
+app.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+/* GET Contact Me page with a title */
 app.get('/contactme', function(req, res, next) {
   res.render('contactme', { title: 'Contact Me' });
 });
 
-// make this file visible within the rest of the application
+// Make this file visible within the rest of the application
 module.exports = app;
